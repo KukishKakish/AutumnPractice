b8bf84057969d7d4afc10e8649eec30279e81881
KukishKakish <154073211+KukishKakish@users.noreply.github.com>
2025-11-10 13:02:51 +0300

    Create Classes.js

---
+++ Classes/Classes.js
@@ -0,0 +1,18 @@
+class Rectangle {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  calculateArea() {
+    return this.width * this.height;
+  }
+
+  calculatePerimeter() {
+    return 2 * (this.width + this.height);
+  }
+}
+
+const rectangle = new Rectangle(5, 10);
+console.log("Площадь:", rectangle.calculateArea());
+console.log("Периметр:", rectangle.calculatePerimeter());
